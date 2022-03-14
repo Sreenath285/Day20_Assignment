6ce78649d4d42782e7dfd28f2cbbc022d843ba99
Sreenath285 <sreenath.285@gmail.com>
2022-03-14 13:12:53 +0530

    Validating user's email id

---
+++ UserRegistrationRegex/src/UserRegistration.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public String regexPattern;
+
+    /***
+     * validateFirstName method created to check given first name matches the pattern or not
+     */
+    public void validateFirstName () {
+
+        regexPattern = "^[A-Z]{1}[a-z]{2,}$";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first name : ");
+        String firstName = sc.next();
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(firstName);
+        if (matcher.matches()) {
+            System.out.println("Valid input");
+        }
+        else {
+            System.out.println("Invalid input");
+        }
+    }
+
+    /***
+     * validateLastName method created to check given last name matches the pattern or not
+     */
+    public void validateLastName() {
+
+        regexPattern = "^[A-Z]{1}[a-z]{2,}$";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter last name : ");
+        String lastName = sc.next();
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(lastName);
+        if (matcher.matches()) {
+            System.out.println("Valid input");
+        }
+        else {
+            System.out.println("Invalid input");
+        }
+    }
+
+    /***
+     * validateEmail method created to check given email matches the pattern or not
+     */
+    public void validateEmail() {
+
+        regexPattern = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}+(.[a-z]{2}+.[a-z]{2}+)*$";
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter email id : ");
+        String email = sc.next();
+        Pattern pattern = Pattern.compile(regexPattern);
+        Matcher matcher = pattern.matcher(email);
+        if (matcher.matches()) {
+            System.out.println("Valid input");
+        }
+        else {
+            System.out.println("Invalid input");
+        }
+    }
+}
