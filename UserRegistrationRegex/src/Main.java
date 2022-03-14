d8e2366dd46d64e6792023cf710d2f26092f9a0f
Sreenath285 <sreenath.285@gmail.com>
2022-03-14 13:15:40 +0530

    Validating user's mobile number

---
+++ UserRegistrationRegex/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class Main {
+
+    /***
+     * main method for manipulation
+     * @param args - default param(not used)
+     */
+    public static void main(String[] args) {
+
+        /***
+         * PROCEDURE
+         * 1. created user registration
+         * 2. while loop to iterate choice
+         */
+
+        /***
+         * 1. created user registration
+         */
+        UserRegistration userRegistration = new UserRegistration();
+
+        /***
+         * 2. while loop to iterate choice
+         */
+        while (true) {
+            System.out.println("\n1. Validate first name \n2. Validate last name" +
+                    "\n3. Validate email id \n4. Validate mobile number" +
+                    "\n5. Exit");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter choice : ");
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1 :
+                    userRegistration.validateFirstName();
+                    break;
+                case 2 :
+                    userRegistration.validateLastName();
+                    break;
+                case 3 :
+                    userRegistration.validateEmail();
+                    break;
+                case 4 :
+                    userRegistration.validateMobileNumber();
+                    break;
+                case 5 :
+                    System.exit(0);
+                    break;
+                default :
+                    System.out.println("Invalid input");
+                    break;
+            }
+        }
+    }
+}
